377cf7f2efe7ad1033d5b9a667bd51a20643e383
Axel Curmi <axel.curmi@gmail.com>
2021-07-16 10:34:36 +0200

    add commands memory results

---
+++ src/max_memory_usage.py
@@ -0,0 +1,24 @@
+def main():
+    import argparse
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("mprofile_file", type=argparse.FileType('r'),
+        help="Python memory profiler data file")
+
+    args = argparser.parse_args()
+
+    mem_max = 0
+    for line in args.mprofile_file:
+        fields = line.split(" ")
+
+        if fields[0] != "MEM":
+            continue
+        
+        mem = float(fields[1])
+
+        if mem > mem_max:
+            mem_max = mem
+    print("MAX MiB: {}".format(mem_max))
+
+if __name__ == "__main__":
+    main()
