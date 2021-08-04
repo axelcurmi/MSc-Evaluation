78de6a83d2e2733cc2653f192816819679d060b7
Axel Curmi <axel.curmi@gmail.com>
2021-08-04 13:40:21 +0200

    add files test results

--- src/experiment_runner.py
+++ src/experiment_runner.py
@@ -35,6 +35,10 @@ def main():
         help="Runs the Paramiko experiments with RV instrumentation")
     argparser.add_argument("--with-secube", default=False, action="store_true",
         help="Runs the Paramiko experiments using the SEcube HSM")
+    argparser.add_argument("--out", help="Specify an output directory")
+    argparser.add_argument("--no-calc-stats", default=False,
+        action="store_true",
+        help="Determines whether the AVG and STDEV stats are auto-calculated")
     argparser.add_argument("--unbuffered", default=False,
         action="store_true",
         help="Determines whether the saving of RV events is unbuffered or not")
@@ -52,12 +56,16 @@ def main():
     #       - out/20210713_commands_T
     #       - out/20210713_shell_F
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    out_dir = os.path.join(
-        config["out_dir"],
-        "{}_{}_{}_{}".format(timestamp,
-                          instruction["type"],
-                          str(args.with_instrumentation)[0],
-                          str(args.with_secube)[0]))
+    if args.out:
+        out_dir = os.path.join(args.out,
+            "{}_{}".format(str(args.with_instrumentation)[0],
+                              str(args.with_secube)[0]))
+    else:
+        out_dir = os.path.join(config["out_dir"],
+            "{}_{}_{}_{}".format(timestamp,
+                            instruction["type"],
+                            str(args.with_instrumentation)[0],
+                            str(args.with_secube)[0]))
 
     # Weave aspects if we want to add instrumentation
     if args.with_instrumentation:
@@ -66,14 +74,22 @@ def main():
     for experiment in instruction["experiments"]:
         dest_dir = os.path.join(out_dir, experiment["name"])
         
+        offset = 0
         if not os.path.exists(dest_dir):
             os.makedirs(dest_dir)
+        else:
+            while True:
+                if os.path.exists(os.path.join(dest_dir, f"{offset}.json")):
+                    offset += 1
+                else:
+                    break
 
         print("Running experiment with {} repetitions".format(
             experiment["repetitions"]))
         for i in range(experiment["repetitions"]):
             if args.with_instrumentation:
-                trace_stream = open(os.path.join(dest_dir, f"{i}.json"), "w+b")
+                trace_stream = open(
+                    os.path.join(dest_dir, f"{i + offset}.json"), "w+b")
                 if not args.unbuffered:
                     trace_stream.write(b"[]")
                     rv.set_buffered_stream(trace_stream)
@@ -97,7 +113,8 @@ def main():
                 rv.event_id = 0
                 trace_stream.close()
 
-        util.add_stats(dest_dir)
+        if not args.no_calc_stats:
+            util.add_stats(dest_dir)
 
 if __name__ == "__main__":
     main()
