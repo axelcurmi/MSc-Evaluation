7e06277e635c20ccafa740e29b84e91c1f35a11f
Axel Curmi <axel.curmi@gmail.com>
2021-08-05 17:40:18 +0200

    add 256B results

---
+++ scripts/Docker_SEcube_256B_Full.ps1
@@ -0,0 +1,25 @@
+$out = 'out/Docker_256B_Full';
+
+$experiments = @("256B-1", "256B-10", "256B-100", "256B-1000")
+
+foreach ($i in $experiments) {
+    # [-] RV
+    # [+] SEcube
+    New-Item "${out}/F_T/${i}" -ItemType Directory -ErrorAction SilentlyContinue
+    mprof run --out "${out}/F_T/${i}/F_T_0.dat" src/experiment_runner.py `
+    config.json "instructions/memory/docker/docker_${i}.json" --with-secube --no-calc-stats --out ${out}
+    mprof run --out "${out}/F_T/${i}/F_T_0.dat" src/experiment_runner.py `
+    config.json "instructions/memory/docker/docker_${i}.json" --with-secube --no-calc-stats --out ${out}
+    mprof run --out "${out}/F_T/${i}/F_T_0.dat" src/experiment_runner.py `
+    config.json "instructions/memory/docker/docker_${i}.json" --with-secube --out ${out}
+
+    # [+] RV
+    # [+] SEcube
+    New-Item "${out}/T_T/${i}" -ItemType Directory -ErrorAction SilentlyContinue
+    mprof run --out "${out}/T_T/${i}/T_T_0.dat" src/experiment_runner.py `
+    config.json "instructions/memory/docker/docker_${i}.json" --with-instrumentation --with-secube --no-calc-stats --out ${out}
+    mprof run --out "${out}/T_T/${i}/T_T_0.dat" src/experiment_runner.py `
+    config.json "instructions/memory/docker/docker_${i}.json" --with-instrumentation --with-secube --no-calc-stats --out ${out}
+    mprof run --out "${out}/T_T/${i}/T_T_0.dat" src/experiment_runner.py `
+    config.json "instructions/memory/docker/docker_${i}.json" --with-instrumentation --with-secube --out ${out}
+}
