1afa60219ebb6f9fec8e70af6d545d7b53b75adf
Axel Curmi <axel.curmi@gmail.com>
2021-08-05 23:42:33 +0200

    add docker 1MB results

---
+++ scripts/Docker_NoSEcube_1MB_Full.ps1
@@ -0,0 +1,25 @@
+$out = 'out/Docker_1MB_Full';
+
+$experiments = @("1MB-1", "1MB-10", "1MB-100", "1MB-1000")
+
+foreach ($i in $experiments) {
+    # [-] RV
+    # [-] SEcube
+    New-Item "${out}/F_F/${i}" -ItemType Directory -ErrorAction SilentlyContinue
+    mprof run --out "${out}/F_F/${i}/F_F_0.dat" src/experiment_runner.py `
+    config.json "instructions/memory/docker/docker_${i}.json" --no-calc-stats --out ${out}
+    mprof run --out "${out}/F_F/${i}/F_F_1.dat" src/experiment_runner.py `
+    config.json "instructions/memory/docker/docker_${i}.json" --no-calc-stats --out ${out}
+    mprof run --out "${out}/F_F/${i}/F_F_2.dat" src/experiment_runner.py `
+    config.json "instructions/memory/docker/docker_${i}.json" --out ${out}
+
+    # [+] RV
+    # [-] SEcube
+    New-Item "${out}/T_F/${i}" -ItemType Directory -ErrorAction SilentlyContinue
+    mprof run --out "${out}/T_F/${i}/T_F_0.dat" src/experiment_runner.py `
+    config.json "instructions/memory/docker/docker_${i}.json" --with-instrumentation --no-calc-stats --out ${out}
+    mprof run --out "${out}/T_F/${i}/T_F_1.dat" src/experiment_runner.py `
+    config.json "instructions/memory/docker/docker_${i}.json" --with-instrumentation --no-calc-stats --out ${out}
+    mprof run --out "${out}/T_F/${i}/T_F_2.dat" src/experiment_runner.py `
+    config.json "instructions/memory/docker/docker_${i}.json" --with-instrumentation --out ${out}
+}
