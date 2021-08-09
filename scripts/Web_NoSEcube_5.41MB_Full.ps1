e00c3f12561035028372e3b7fdeb03501b1b725c
Axel Curmi <axel.curmi@gmail.com>
2021-08-09 17:02:03 +0200

    add web 5.41 tests

---
+++ scripts/Web_NoSEcube_5.41MB_Full.ps1
@@ -0,0 +1,21 @@
+$out = 'out/Web_5.41MB_Full';
+
+$experiments = @("5.41MB-1", "5.41MB-10", "5.41MB-100", "5.41MB-1000")
+
+foreach ($i in $experiments) {
+    # [-] RV
+    # [-] SEcube
+    New-Item "${out}/F_F/${i}" -ItemType Directory -ErrorAction SilentlyContinue
+    mprof run --out "${out}/F_F/${i}/F_F_0.dat" src/experiment_runner.py `
+    config.json "instructions/memory/web/web_${i}.json" --no-calc-stats --out ${out}
+    mprof run --out "${out}/F_F/${i}/F_F_1.dat" src/experiment_runner.py `
+    config.json "instructions/memory/web/web_${i}.json" --out ${out}
+
+    # [+] RV
+    # [-] SEcube
+    New-Item "${out}/T_F/${i}" -ItemType Directory -ErrorAction SilentlyContinue
+    mprof run --out "${out}/T_F/${i}/T_F_0.dat" src/experiment_runner.py `
+    config.json "instructions/memory/web/web_${i}.json" --with-instrumentation --no-calc-stats --out ${out}
+    mprof run --out "${out}/T_F/${i}/T_F_1.dat" src/experiment_runner.py `
+    config.json "instructions/memory/web/web_${i}.json" --with-instrumentation --out ${out}
+}
