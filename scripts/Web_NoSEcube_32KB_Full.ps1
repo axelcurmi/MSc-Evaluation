f18691d044fa069e3655fb8237391327a715ce54
Axel Curmi <axel.curmi@gmail.com>
2021-08-10 16:27:01 +0200

    add 32KB web results

---
+++ scripts/Web_NoSEcube_32KB_Full.ps1
@@ -0,0 +1,21 @@
+$out = 'out/Web_32KB_Full';
+
+$experiments = @("32KB-1", "32KB-10", "32KB-100", "32KB-1000")
+
+foreach ($i in $experiments) {
+    # [-] RV
+    # [-] SEcube
+    New-Item "${out}/F_F/${i}" -ItemType Directory -ErrorAction SilentlyContinue
+    mprof run --out "${out}/F_F/${i}/F_F_0.dat" src/experiment_runner.py `
+    config.json "instructions/memory/web/web_${i}.json" --no-calc-stats --out ${out}
+    mprof run --out "${out}/F_F/${i}/F_F_1.dat" src/experiment_runner.py `
+    config.json "instructions/memory/web/web_${i}.json" --out ${out}
+
+    # [+] RV
+    # [-] SEcube
+    New-Item "${out}/T_F/${i}" -ItemType Directory -ErrorAction SilentlyContinue
+    mprof run --out "${out}/T_F/${i}/T_F_0.dat" src/experiment_runner.py `
+    config.json "instructions/memory/web/web_${i}.json" --with-instrumentation --no-calc-stats --out ${out}
+    mprof run --out "${out}/T_F/${i}/T_F_1.dat" src/experiment_runner.py `
+    config.json "instructions/memory/web/web_${i}.json" --with-instrumentation --out ${out}
+}
